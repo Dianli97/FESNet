ba1dbb6e7ff6bed01052708f3aa3dbbb91040c3e
Dianli97 <136307555+Dianli97@users.noreply.github.com>
2025-01-10 15:59:02 +0900

    Update train.py

--- train.py
+++ train.py
@@ -5,9 +5,7 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 import config
-from model import I2CNet  # 假设I2CNet定义在i2cnet模块中
-# from model_noatt import I2CNet
-# from model_noatti2c import I2CNet
+from model import I2CNet
 from dataset import EMGDataset
 from preprocessor import preprocess_data
 import pickle
@@ -15,12 +13,11 @@ import time
 import pandas as pd
 
 
-# 定义训练参数
 batch_size = config.batch_size
 epochs = config.num_epochs
 learning_rate = config.learning_rate
-window_size = config.window_size  # 200长度的window size
-train_path = '/home/ld/python/db8_data/train/'
+window_size = config.window_size  
+train_path = 'XX' #PATH TO YOUR TRAIN DATASET
 loss_log = []
 width = config.width
 acc = config.acc
@@ -30,7 +27,6 @@ if config.width != 1:
 else:
     task = 'seg'
 
-# 预处理数据
 subj_list = [0,1,2,3,4,5,6,7,8,9,10,11]
 # subj_list = [6]
 ex_list = [0]
@@ -39,13 +35,11 @@ acq_list = [0, 1]
 x_data, y_data = preprocess_data(train_path, subj_list, ex_list, acq_list, window_size)
 # print(f'Original labels - Min: {y_data.min().item()}, Max: {y_data.max().item()}')
 
-# 保存归一化模型
 # with open('scaler_x.pkl', 'wb') as f:
 #     pickle.dump(scaler_x, f)
 # with open('scaler_y.pkl', 'wb') as f:
 #     pickle.dump(scaler_y, f)
 
-# 创建数据集和数据加载器
 dataset = EMGDataset(x_data, y_data)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
@@ -59,27 +53,22 @@ class CombinedLoss(nn.Module):
         self.weight_ce = weight_ce
 
     def forward(self, outputs, targets):
-        # 调整为 [batchsize * channels * length, num_class]
+        # To [batchsize * channels * length, num_class]
         outputs_reshaped = outputs.permute(0, 1, 3, 2).contiguous().view(-1, outputs.size(2))
-        # 调整为 [batchsize * channels * length]
+        # To [batchsize * channels * length]
         targets_reshaped = targets.view(-1).long()
 
-        # 计算交叉熵损失
         ce = self.ce_loss(outputs_reshaped, targets_reshaped)
         
-        # 在 num_class 维度取 argmax，使数据形状变为 [batchsize, channels, length]
         outputs_max = outputs.argmax(dim=2)
         
-        # MAE损失
         mae = self.mae_loss(outputs_max.float(), targets.float())
         
-        # 计算皮尔逊相关系数
         vx = outputs_max.float() - torch.mean(outputs_max.float())
         vy = targets.float() - torch.mean(targets.float())
         cost = torch.sum(vx * vy) / (torch.sqrt(torch.sum(vx ** 2)) * torch.sqrt(torch.sum(vy ** 2)))
         pearson_correlation = 1 - cost  # 1 minus the Pearson correlation coefficient
         
-        # 结合三种损失
         total_loss = self.weight_mae * mae + self.weight_pearson * pearson_correlation + self.weight_ce * ce
         if task == 'seg':
             return total_loss
@@ -87,18 +76,14 @@ class CombinedLoss(nn.Module):
             return ce
 
 
-# 使用自定义损失函数
 criterion = CombinedLoss(weight_pearson=0.33, weight_mae=0.33, weight_ce=0.33)
 
-# 初始化I2CNet模型
 model = I2CNet(in_planes=16)
 
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# 设置学习率调度器
 scheduler = StepLR(optimizer, step_size=20, gamma=0.9)
 
-# 检查GPU可用性
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
@@ -107,13 +92,11 @@ model.to(device)
 min_val = -95
 max_val = 139
 
-# 训练I2CNet模型
 for epoch in range(epochs):
     model.train()
     running_loss = 0.0
     for inputs, labels in dataloader:
         inputs, labels = inputs.to(device), labels.to(device)
-        # 在reshape前检查标签的最小值和最大值
         # print(f'Original labels - Min: {labels.min().item()}, Max: {labels.max().item()}')
         labels = (labels - min_val) / (max_val - min_val)
         if width > 1:
@@ -122,7 +105,7 @@ for epoch in range(epochs):
             # print('labels after',labels.shape)
         if acc is not None:
             labels = torch.floor(labels * (180 / acc))
-            # labels[labels < 0] = 0  # 如果 labels 为负值，则设为 0
+            # labels[labels < 0] = 0  
         optimizer.zero_grad()
 
         outputs = model(inputs)
@@ -132,7 +115,6 @@ for epoch in range(epochs):
 
         running_loss += loss.item()
     
-    # 在每个epoch结束时更新学习率
     scheduler.step()
     epoch_loss = running_loss/len(dataloader)
     loss_log.append(epoch_loss)
@@ -140,9 +122,6 @@ for epoch in range(epochs):
 
 print("Finished Training")
 
-# 保存训练好的模型
 logs = pd.DataFrame(loss_log, columns=['losses'])
 logs.to_excel(config.save_path+'loss_logs.xlsx', index=False)
 torch.save(model.state_dict(), config.save_path + f'model_{time.time()}.pth')
-
-# 对训练输出进行一些调整,以适应交叉熵损失函数
